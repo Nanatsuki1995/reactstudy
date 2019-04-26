cbec3247a7444f1b0ae620d7ae266b57bd8b5033
Nanatsuki <514948303@qq.com>
2019-04-26 11:40:20 +0800

    react基础学习完成

---
+++ stateup/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Calculator from './components/calculator';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Calculator />
+            </div>
+        )
+    }
+}
+
+export default App
